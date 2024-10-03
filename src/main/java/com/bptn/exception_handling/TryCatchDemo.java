d14cb8e3e814541c2f9aff8748ee675838a6f3b9
AsaJules <eziamakaifesinachi@gmail.com>
2024-10-03 16:20:04 -0400

    initial commit

---
+++ src/main/java/com/bptn/exception_handling/TryCatchDemo.java
@@ -0,0 +1,13 @@
+package com.bptn.exception_handling;
+
+class TryCatchDemo {
+	
+	try{
+	// code that might throw an exception	
+	}catch(ExceptionType e) {
+	// code to handle the exception	
+	}finally {
+	// code that will always execute	
+	}
+
+}
